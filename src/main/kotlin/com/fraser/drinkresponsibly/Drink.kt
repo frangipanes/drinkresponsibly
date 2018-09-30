4216e716a366c45e4a5fbe8af5806c8004142570
Fraser Kettle <fraser.kettle@masabi.com>
2018-09-30 15:09:30 +0100

    Add stepsize parameter to drink classes

--- src/main/kotlin/com/fraser/drinkresponsibly/Drink.kt
+++ src/main/kotlin/com/fraser/drinkresponsibly/Drink.kt
@@ -3,9 +3,23 @@ package com.fraser.drinkresponsibly
 interface Drink {
     val name: String
     val size: Double
+    val stepSize: Double
 }
 
-data class Bottle(override val name: String, override val size: Double) : Drink
-data class Can(override val name: String, override val size: Double) : Drink
-data class Pint(override val name: String, override val size: Double) : Drink
-data class Wine(override val name: String, override val size: Double) : Drink
+data class Bottle(override val name: String, override val size: Double) : Drink {
+    override val stepSize: Double
+        get() = 1.0
+}
+
+data class Can(override val name: String, override val size: Double) : Drink {
+    override val stepSize: Double
+        get() = 1.0
+}
+data class Pint(override val name: String, override val size: Double) : Drink {
+    override val stepSize: Double
+        get() = 0.5
+}
+data class Wine(override val name: String, override val size: Double) : Drink {
+    override val stepSize: Double
+        get() = 1.0
+}
